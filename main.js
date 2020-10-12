b6b7cba580b502ae019b7f32335ae5c3bc7912fa
Gregory.Johnson <3531op@gmail.com>
2020-10-12 12:24:55 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -1,6 +1,89 @@
 /******************
  * YOUR CODE HERE *
 ******************/
+function doubleAll(num) {
+  const newDouble = []
+  for (let i = 0; i < num.length; i++) {
+    newDouble.push(num[i] * 2)
+  }
+  return newDouble
+}
+function yelledGreetings(name) {
+  const yelling = [];
+  for (let i = 0; i < name.length; i++) {
+    yelling.push(name[i] + '!')
+  }
+  return yelling
+}
+function absoluteValues(num) {
+  const newNumber = []; // Creating new array to push items into
+  for (let i = 0; i < num.length; i++) { // Loops through length of array specified
+
+    bestNumber = Math.abs(num[i])
+    newNumber.push(bestNumber);
+  }
+  return (newNumber)
+}
+function upperCaseFirstLetters(name) {
+  const buddyGuy = [];
+  for (let i = 0; i < name.length; i++) {
+
+    notYourBuddyGuy = name[i].charAt(0).toUpperCase();
+    notYourFriendGuy = name[i].slice(1).toLowerCase()
+    buddyGuy.push(notYourBuddyGuy + notYourFriendGuy)
+  }
+  return buddyGuy
+}
+function changeToInitials(userArray) {
+  const newArray = [];
+  for (let i = 0; i < userArray.length; i++) {
+    notTheNewArray = userArray[i].slice(0, 1)
+    spaceCounter = userArray[i].indexOf(' ')
+    alsoNotTheNewArray = userArray[i].slice((spaceCounter + 1), (spaceCounter + 2))
+    newArray.push(notTheNewArray + alsoNotTheNewArray)
+  }
+  return newArray
+}
+function doubleOdd(array) {
+  const array2 = [];
+  for (let i = 0; i < array.length; i++) {
+    
+    if (array[i] * 10 % 10 !== 0) {
+      array2.push(array[i])
+    
+    } else if (array[i] % 2 === 0) {
+      array2.push(array[i]);
+
+    } else if (array[i] % 2 !== 0) {
+      array2.push(array[i] * 2)
+    }
+  }
+  return (array2)
+}
+function add1ToLeft(crazyGlue) {
+const notThatSticky = []
+  for (let i = 0; i < crazyGlue.length; i++)
+if (crazyGlue[i] < 0) {
+    notThatSticky.push (-1 * ('1' + crazyGlue[i] * - 1))
+  
+  } else if (crazyGlue[i] >= 0) { 
+    notThatSticky.push (1 * ('1' + crazyGlue[i]))
+  }
+return (notThatSticky) 
+}
+// function add1ToLeft(crazyGlue) {
+//   const notThatSticky = []
+//     for (let i = 0; i < crazyGlue.length; i++)
+//   if (crazyGlue[i] < 0) {
+//       notThatSticky.push((-1 *('1' + crazyGlue[i] * - 1)))
+    
+//     } else if (crazyGlue[i] >= 0) { 
+//       notThatSticky.push(Math.abs('1' + crazyGlue[i]))
+//     }
+//   return (notThatSticky) 
+
+
+// return superGlue
 
 
 
